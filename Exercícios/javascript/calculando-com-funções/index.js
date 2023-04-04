14fafeb0cb223484830b42329de71cddc50ab82d
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-04-04 20:30:31 -0300

    Resolução do exercício de funções em JS.

---
+++ Exercícios/javascript/calculando-com-funções/index.js
@@ -0,0 +1,27 @@
+function somar(numero1, numero2){
+    return numero1 + numero2
+}
+
+let soma = somar(3, 5)
+console.log(`O resultado da soma dos dois números é: ${soma}`)
+
+function subtrair(numero1, numero2){
+    return numero1 - numero2
+}
+
+let subtracao = subtrair(10,7)
+console.log(`O resultado da subtração dos dois números é: ${subtracao}`)
+
+function multiplicar(numero1, numero2){
+    return numero1 * numero2
+}
+
+let multiplicacao = multiplicar(6,5)
+console.log(`O resultado da multiplicação dos dois números é: ${multiplicacao}`)
+
+function dividir(numero1, numero2){
+    return numero1 / numero2
+}
+
+let divisao = dividir(10, 2)
+console.log(`O resultado da divisão dos dois números é: ${divisao}`)
