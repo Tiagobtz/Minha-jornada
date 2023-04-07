919034188744b94c99637017895ab404ba4049a7
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-04-07 20:55:44 -0300

    Arrow functions

---
+++ 08-javascript-intermediário/04-arrow-functions/index.js
@@ -0,0 +1,10 @@
+// let resultadoDaSoma = (numero1, numero2) => {
+//     return numero1 + numero2
+// }
+// console.log(resultadoDaSoma(10, 2))
+
+// let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
+// console.log(resultadoDaSoma(10, 2))
+
+const incentivarQuester = nomeQuester => console.log(`Parabéns ${nomeQuester}, você chegou ao módulo de JS intermediário.`)
+incentivarQuester('Tiago')
