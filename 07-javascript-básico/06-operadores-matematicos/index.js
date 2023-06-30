064b6995f55b2c500aafff768a0af2b3462cf91e
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-06-30 05:50:39 -0300

    Atualizando

--- 07-javascript-básico/06-operadores-matematicos/index.js
+++ 07-javascript-básico/06-operadores-matematicos/index.js
@@ -1,3 +1,4 @@
+/*
 let primeiroValor = 10
 let segundoValor = 5
 
@@ -6,3 +7,32 @@ console.log(primeiroValor - segundoValor)
 console.log(primeiroValor * segundoValor)
 console.log(primeiroValor / segundoValor)
 console.log(primeiroValor % segundoValor)
+*/
+
+//                                    Operadores aritméticos na atribuição de valores
+
+// let numeroDeTeste = 10
+
+// numeroDeTeste += 5             =>     É o mesmo que o valor da variável numeroDeTeste + 5
+// console.log(numeroDeTeste)
+
+// numeroDeTeste -= 5             =>     É o mesmo que o valor da variável numeroDeTeste - 5
+// console.log(numeroDeTeste)
+
+// numeroDeTeste *= 5             =>     É o mesmo que o valor da variável numeroDeTeste * 5
+// console.log(numeroDeTeste)
+
+// numeroDeTeste /= 5             =>     É o mesmo que o valor da variável numeroDeTeste / 5
+// console.log(numeroDeTeste)
+
+// numeroDeTeste %= 5             =>     É o mesmo que o valor da variável numeroDeTeste % 5
+// console.log(numeroDeTeste)
+
+// Pré-incremento
+// console.log(++ numeroDeTeste)    // O resultado será o valor da variável numeroDeTeste + 1 (dentro do print) 
+// Pós-incremento
+// console.log(numeroDeTeste ++)    // O resultado será o valor da variável numeroDeTeste + 1 (após a execução)
+// Pré-decremento
+// console.log(-- numeroDeTeste)    // O resultado será o valor da variável numeroDeTeste + 1 (dentro do print)
+// Pós-decremento
+// console.log(numeroDeTeste --)    // O resultado será o valor da variável numeroDeTeste + 1 (após a execução)
