6c08ce95e6e64470109edcd27c5996c48da9cc05
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-07-25 21:51:22 -0300

    Aprendendo um pouco mais sobre funções para operações matemáticas.

--- 08-javascript-intermediário/03-funcoes/06-funcoes-para-operacoes-matematicas/index.js
+++ 08-javascript-intermediário/03-funcoes/06-funcoes-para-operacoes-matematicas/index.js
@@ -13,3 +13,9 @@ console.log(Math.round(numero))
 // A função random() sorteia e retorna um valor, como no exemplo abaixo sorteamos um número entre 0 e 10. Veja que também utilizamos a função floor() para que o número sorteado seja inteiro.
 let numeroSorteado = 10
 console.log(Math.floor(Math.random() * numeroSorteado + 1))
+
+// A função abaixo pode ser utilizada para calcular a potência de um número. Neste exemplo o valor a ser printado será 81, pois 9 ao quadrado é o mesmo que (9 * 9) que terá 81 como resultado.
+console.log(Math.pow(9,2))
+
+// A função abaixo irá calcular a raíz quadrada de qualquer número que seja inserido como parâmetro entre os parênteses. Neste exemplo, o resultado será 9.
+console.log(Math.sqrt(81))
