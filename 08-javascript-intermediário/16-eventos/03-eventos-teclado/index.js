f3ce2cb1c79ff519d06a61ed4c6e24b3dab44e9b
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-07-27 05:59:51 -0300

    Atualizando

---
+++ 08-javascript-intermediário/16-eventos/03-eventos-teclado/index.js
@@ -0,0 +1,5 @@
+function acao1(){
+    alert('Evento disparado')
+}
+
+// Os eventos são executados ao pressionarmos as teclas do teclado. Cada um desses eventos possuem um funcionalidade diferente.
