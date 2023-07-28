87882bb6ea95c838bebffc2dabfd045342e53a10
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-07-28 05:08:36 -0300

    Atualizando

---
+++ 08-javascript-intermediário/16-eventos/05-eventos-formulario/index.js
@@ -0,0 +1,9 @@
+function acao1(){
+    console.log('Elemento com foco')
+}
+
+function acao2(){
+    console.log('Estado modificado')
+}
+
+// Estes são alguns exemplos de eventos que podemos adcionar a elementos que compõe um formulário.
