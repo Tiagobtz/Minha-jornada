3f9eb71fbcb36aeea13bf00af4cf7e1ff4750c69
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-07-28 06:09:07 -0300

    Atualizando

---
+++ 08-javascript-intermediário/14-DOM/04-manipulado-valores-de-inputs-text/index.js
@@ -0,0 +1,29 @@
+function adicaoDeCaractere(){
+    // Selecionando o valor digitado no input
+    let caractere = document.getElementById('entrada').value
+
+    // Limpar o campo de digitação
+    document.getElementById('entrada').value = ''
+
+    // Limpar os espaços em branco nas extremidades da string
+    caractere.trim()
+
+    switch (caractere) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            // Adcionando o caractere referente ao campo de Números
+            document.getElementById('numeros').value += caractere
+            break;
+        default:
+            // Adcionando o caractere referente ao campo de Letras
+            document.getElementById('letras').value += caractere
+    }
+}
