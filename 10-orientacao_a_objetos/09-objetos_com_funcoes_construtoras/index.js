00689c68919c6b912537fa389f630f031140f063
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-09-05 21:56:53 -0300

    Atualizando

---
+++ 10-orientacao_a_objetos/09-objetos_com_funcoes_construtoras/index.js
@@ -0,0 +1,26 @@
+let Carro = function(){
+    this.cor = 'Amarelo'
+    this.modelo = 'Chevette'
+    this.velocidadeAtual = 0
+    this.velocidadeMaxima = 180
+
+    this.acelerar = function(){
+        let velocidade = this.getVelocidadeAtual() + 10
+        this.setVelocidadeAtual(velocidade)
+    }
+
+    this.getVelocidadeAtual = function(){
+        return this.velocidadeAtual
+    }
+
+    this.setVelocidadeAtual = function(velocidadeAtual){
+        this.velocidadeAtual = velocidadeAtual
+    }
+}
+
+let carro = new Carro()
+
+console.log(carro.acelerar())
+console.log(carro.acelerar())
+console.log(carro.acelerar())
+console.log(`A velocidade atual é: ${carro.getVelocidadeAtual()}`)
