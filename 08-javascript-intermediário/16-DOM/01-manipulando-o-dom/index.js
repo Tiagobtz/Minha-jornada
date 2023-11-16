9a98d2d49c68d4c8124b9b23e9ccdfd2c7d0d419
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-11-15 22:24:03 -0300

    Atualizando

--- 08-javascript-intermediário/16-DOM/01-manipulando-o-dom/index.js
+++ 08-javascript-intermediário/16-DOM/01-manipulando-o-dom/index.js
@@ -1,3 +1,5 @@
+/*
+
 // SELECIONANDO UTILIZANDO O MÉTODO PARA SELECIONAR O DOCUMENTO ATRAVÉS DO ID
 let p = document.getElementById('paragrafo1')
 console.log(p)
@@ -13,3 +15,25 @@ console.log(inputPorTag)
 // SELECIONANDO UTILIZANDO O MÉTODO PARA SELECIONAR O DOCUMENTO COM MAIS POSSIBILIDADES
 let inputPorName = document.querySelectorAll("input[name='email']")
 console.log(inputPorName[0])
+*/
+
+function testar(){
+    let teste = document.querySelector("#teste")
+    let ul = teste.querySelector("ul")
+
+    let newUl = document.createElement("ul")
+    ul.after(newUl)
+
+    for (let i = 0; i < 5; i ++){
+        let newLinha = document.createElement("li")
+        newLinha.innerHTML = `Item add ${i + 1}`
+        newUl.appendChild(newLinha)
+    }
+
+    /*
+    novaLinha = document.createElement("li") // Para criar um novo elemento
+    novaLinha.innerText = "Item adcionado"
+    ul.prepend(novaLinha) // Para adcionar o elemento no início
+    ul.appendChild(novaLinha) // Para adcionar o elemento no final
+    */
+}
