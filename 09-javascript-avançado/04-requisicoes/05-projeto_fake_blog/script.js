7067320a6aa70a0e4690717943ca2985fc8764a2
Tiago Henrique <tiagomacedoh015@gmail.com>
2024-01-18 22:10:27 -0300

    Atualizando

--- 09-javascript-avançado/04-requisicoes/05-projeto_fake_blog/script.js
+++ 09-javascript-avançado/04-requisicoes/05-projeto_fake_blog/script.js
@@ -1,20 +1,48 @@
 async function carregarPosts(){
-    let posts = document.querySelector(".posts")
-    posts.innerHTML = `Carregando...`
-
     let resposta = await fetch('https://jsonplaceholder.typicode.com/posts')
-    let json = await resposta.json()
+    let posts = document.querySelector('.posts')
+    posts.innerHTML = 'Carregando...'
     
+    let json = await resposta.json()
+
     if (json.length > 0){
         posts.innerHTML = ''
-
-        for(let i in json){
-            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr></div>`
+        for (i in json) {
+            let postHtml = `<div><h1>${json[i].title}</h1><hr>${json[i].body}</div>`
             posts.innerHTML += postHtml
         }
-    } else {
-        posts.innerHTML = "Nenhum post encontrado"
+        
     }
 }
+carregarPosts()
 
+async function addNovoPost(titulo, corpo){
+    await fetch('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            title: titulo,
+            body: corpo,
+        })
+    })
+    titulo = document.getElementById('campo-titulo').value = ''
+    corpo = document.getElementById('campo-corpo').value = ''
+    carregarPosts()
+
+}
+
+document.querySelector('#botao-inserir').addEventListener('click', function(){
+    let titulo = document.getElementById('campo-titulo').value
+    let corpo = document.getElementById('campo-corpo').value
+
+    if (titulo && corpo){
+        addNovoPost(titulo, corpo)
+    } else {
+        alert('Preencha todos os campos.')
+    }
+})
 carregarPosts()
