da091c174526dda8718b62eed83050f4ffee03d7
Tiago Henrique <tiagomacedoh015@gmail.com>
2024-01-18 22:08:04 -0300

    Aprendendo React

---
+++ 12-reactJS/02-primeiro-react/src/app/page.tsx
@@ -0,0 +1,14 @@
+import {GeoForms} from "@/components/geoForms"
+
+function Page(){
+  return (
+    <div>
+      <h1 className="font-bold text-5xl">Olá, mundo!</h1>
+      <h3>Outro teste</h3>
+
+      <GeoForms/>
+    </div>
+  );
+}
+
+export default Page
