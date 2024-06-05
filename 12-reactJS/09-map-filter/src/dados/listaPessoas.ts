3e16010a9dfdfcae7edc16e0e818fb1692cb89ed
Tiago Henrique <tiagomacedoh015@gmail.com>
2024-06-04 22:22:37 -0300

    Atualizando

---
+++ 12-reactJS/09-map-filter/src/dados/listaPessoas.ts
@@ -0,0 +1,8 @@
+import { pessoa } from "@/tipos/pessoa";
+
+export const listaPessoas: pessoa[] = [
+    {id: 1, nome: 'Tiago', profissao: 'Programador'},
+    {id: 2, nome: 'Maria', profissao: 'Streamer'},
+    {id: 3, nome: 'João', profissao: 'Programador'},
+    {id: 4, nome: 'Antonio', profissao: 'Professor'},
+]
